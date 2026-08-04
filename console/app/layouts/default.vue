1d5eafd88d6fbbc6afcf4b62b08b72957262ad7a
opeolluwa <adefemiadeoye@yahoo.com>
2026-08-04 19:39:17 +0100

    connect BE+FE

---
+++ console/app/layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <UHeader>
+    <template #left>
+      <NuxtLink to="/">
+        <AppLogo class="w-auto h-6 shrink-0" />
+      </NuxtLink>
+    </template>
+
+    <template #right>
+      <UColorModeButton />
+
+      <UButton
+        to="https://github.com/opeolluwa/unshift"
+        target="_blank"
+        icon="i-simple-icons-github"
+        aria-label="GitHub"
+        color="neutral"
+        variant="ghost"
+      />
+    </template>
+  </UHeader>
+
+  <UMain>
+    <UContainer>
+      <slot />
+    </UContainer>
+  </UMain>
+
+  <USeparator />
+
+  <UFooter>
+    <template #left>
+      <p class="text-sm text-muted">
+        MIT Licensed
+      </p>
+    </template>
+
+    <template #right>
+      <UButton
+        to="https://github.com/opeolluwa/unshift"
+        target="_blank"
+        icon="i-simple-icons-github"
+        aria-label="GitHub"
+        color="neutral"
+        variant="ghost"
+      />
+    </template>
+  </UFooter>
+</template>
